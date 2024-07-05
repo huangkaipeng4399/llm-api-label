163c2d8dff1ec747b1b037e656307b96e7c20da1
黄凯鹏 <huangkaipeng@cn0014006455m.domain.sensetime.com>
2024-07-05 19:50:44 +0800

    debug

--- gptlabel.py
+++ gptlabel.py
@@ -13,29 +13,12 @@ from gpt4access import query_gpt4
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+with open("prompt.txt", "r") as f:
+    prompt = f.read()
 
-def gpt_api_func(input_keywords, input_text, model_name='gpt-4', **kwargs):
-    text = f"""
-你是一个数据清洗人员，需要你要筛选出有问题的文档。请对输入的文档主题词和示例文本内容进行检查。需要检查的维度包括：
-政治敏感：危害国家统一、主权和领土完整,违背宪法确认的根本制度,反对中国共产党的领导,对中国政府或国家利益有影响的言论或信息。宣扬恐怖主义、极端主义思想,包括但不限于颂扬、鼓吹、教唆恐怖活动,宣传极端宗教理念,传播暴力血腥内容等。
-色情：传播淫秽色情信息。
-有毒：包含种族、民族、性别、宗教等歧视性言论,传播仇恨、偏激思想,使用攻击性、贬低性语言。宣扬暴力、残暴行为,如虐待、伤害他人,破坏公共秩序等。
-广告：属于广告营销内容，即具有明确的商业目的，旨在促使他人采取某种行动，如购买产品、服务或参与活动等。
-请根据以下条件进行检查：
-1.主题词是否含有相应内容。
-2.示例文本是否在谈论这些内容。
-如果文本主题词和内容满足上述条件，请对相应维度返回"1"，否则返回"0"。
-输出格式示例(必须按照这个格式输出,否则无法解析，不要返回任何额外的内容):
-{{
-"政治敏感": 1,
-"色情": 1,
-"有毒": 0,
-"广告": 0
-}}
 
-文档主题词：{input_keywords}
-示例文本内容：{input_text}
-"""
+def gpt_api_func(model_name='gpt-4', **kwargs):
+    text = eval(f'f"""{prompt}"""')
     if "client_name" in kwargs.keys() and kwargs["client_name"] == 'azure':
         resp_text = query_gpt4(kwargs["api_key"], kwargs["base_url"], text)
     else:
@@ -54,9 +37,11 @@ def gpt_api_func(input_keywords, input_text, model_name='gpt-4', **kwargs):
     return resp_text
 
 
-def parse_response(gpt_response=''):
+def parse_response(
+    gpt_response='',
+    pattern=r'{\s{0,5}"政治敏感":\s{0,5}\d+,\s{0,5}"色情":\s{0,5}\d+,\s{0,5}"有毒":\s{0,5}\d+,\s{0,5}"广告":\s{0,5}\d+\s{0,5}}'
+):
     try:
-        pattern = r'{\s{0,5}"政治敏感":\s{0,5}\d+,\s{0,5}"色情":\s{0,5}\d+,\s{0,5}"有毒":\s{0,5}\d+,\s{0,5}"广告":\s{0,5}\d+\s{0,5}}'
         matches = re.findall(pattern, gpt_response)
         parse_dict = json.loads(matches[0])
         logging.info(f"parsed_dict:{parse_dict}")
@@ -87,18 +72,25 @@ def relabel_text(input_file,
                 continue
             logging.info(f"Processing line: {cnt}")
             line_dict = json.loads(line.strip())
+            """
+            请根据需要修改以下代码段，
+            并在gpt_api_func中根据你的prompt内容调整要传递给prompt的参数。
+            """
+            #begin
             input_keywords = ",".join(line_dict["Keywords"])
             texts = []
             for i in range(min(len(line_dict["documents"]), 5)):
                 #限定输入文本长度
                 texts.append(line_dict["documents"][i][:500])
             input_text = "\n".join(texts)
+            #end
+
             for _ in range(5):
                 try:
                     time.sleep(0.2)
-                    raw_response = gpt_api_func(input_keywords,
-                                                input_text,
-                                                model_name=model_name,
+                    raw_response = gpt_api_func(model_name=model_name,
+                                                input_keywords=input_keywords,
+                                                input_text=input_text,
                                                 **kwargs)
                     gpt_response = parse_response(raw_response)
                     break
@@ -108,8 +100,10 @@ def relabel_text(input_file,
                         gpt_response = {"error_msg": str(e)}
                         break
                     elif any([
-                            x in str(e) for x in
-                        ['API', 'api', 'access', 'key', 'invalid', 'quota']
+                            x in str(e) for x in [
+                                'API', 'api', 'access', 'key', 'invalid',
+                                'quota', 'uth'
+                            ]
                     ]):
                         return cnt
                     else:
@@ -122,6 +116,7 @@ def relabel_text(input_file,
 
 def get_file_list(input_dir):
     file_list = []
+    print(input_dir)
     if os.path.isfile(input_dir):
         return [input_dir]
     for root, dirs, files in os.walk(input_dir):
@@ -133,6 +128,7 @@ def get_file_list(input_dir):
 def run_label(input_file_path, output_file, start_idx, sleep_time, config):
     file_list = get_file_list(input_file_path)
     cnt = start_idx
+    print(file_list)
     for file_idx, file in enumerate(file_list):  #遍历所有的文件
         if os.path.isfile(file):
             for client_name in config.keys():  #遍历所有的apikey
@@ -160,16 +156,18 @@ def run_label(input_file_path, output_file, start_idx, sleep_time, config):
                         model_name=config[client_name]["model_name"],
                         sleep_time=sleep_time,
                         **kwargs)
-                #正常返回-1.如果返回的是cnt，说明文件在处理到第cnt行时出现了问题。
-                if cnt == -1:
-                    break
-                else:
-                    if cnt > 0:
-                        cnt -= 1
-                    config[client_name]["api_keys"][apikey] = "False"
-                    continue
-            if file_idx != len(file_list) - 1:
-                cnt = 0
+                    #正常返回-1.如果返回的是cnt，说明文件在处理到第cnt行时出现了问题。
+                    if cnt == -1:
+                        break
+                    else:
+                        if cnt > 0:
+                            cnt -= 1
+                        config[client_name]["api_keys"][apikey] = "False"
+                        continue
+            if cnt == -1:
+                break
+        if file_idx != len(file_list) - 1:
+            cnt = 0
     if cnt == -1:
         logging.info("The process is completed!")
     else:
--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 cluster_label() {
-    input_file="../cncode_ctfidf"
-    output_file="../testerr1.jsonl"
+    input_file="./text.jsonl"
+    output_file="./test1.jsonl"
     text_key="content"
     start_idx="0"
 
     python main.py \
+        --input_file $input_file \
         --output_file $output_file \
         --start_idx $start_idx \
         --sleep_time 10 \
-        --input_file $input_file 
 }
-
 cluster_label
